fbe1d42ae17e209e4d67e919cea0bfb41aed67b8
Nadezhda <nadusha955955@gmail.com>
2019-07-31 19:29:14 +0300

    delete

--- homework_7/SQLQuery3.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO Employee (Name, SurName, DepId) VALUES ('Olga', 'Sinicina', '1')
-INSERT INTO Employee (Name, SurName, DepId) VALUES ('Pavel', 'Petrov', '2')
-INSERT INTO Employee (Name, SurName, DepId) VALUES ('Semen', 'Drozdov', '3')
-INSERT INTO Employee (Name, SurName, DepId) VALUES ('Svetlana', 'Gashina', '1')
-INSERT INTO Employee (Name, SurName, DepId) VALUES ('Polya', 'Popova', '1')
